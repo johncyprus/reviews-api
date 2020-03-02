98baaab102a00e5c6fb6ef999526b374ba9e426d
John Lee <johnjun.lee91@gmail.com>
2020-03-02 17:17:19 -0500

    Adjust sequelize to use environment variables for deployment

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv').config({path: __dirname + '/.env'});
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+
+// test to see if process.env is  reading .env file correctly
+if (dotenv.error) {throw dotenv.error};
 
 // provide data for requests and responses
 app.use(morgan('dev'));
